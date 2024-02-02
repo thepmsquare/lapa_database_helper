26c0982b4a05fa6cf36bf60f9e4309d2ed386b97
Lav Sharma <lavsharma2016@gmail.com>
2024-02-02 22:16:20 +0530

    lapa_commons is used for reading configuration

--- lapa_database_helper/configuration.py
+++ lapa_database_helper/configuration.py
@@ -1,14 +1,11 @@
-import configparser
 import os
 
-config = configparser.ConfigParser()
-config_file_path = (
-    os.path.dirname(os.path.abspath(__file__)) + os.sep + "data" + os.sep + "config.ini"
-)
-config.read(config_file_path)
+from lapa_commons.main import read_configuration_from_file_path
+
+config_file_path = os.path.dirname(os.path.abspath(__file__)) + os.sep + "data" + os.sep + "config.ini"
+ldict_configuration = read_configuration_from_file_path(config_file_path)
 
 # get all vars and typecast
-
-config_str_lapa_database_protocol = config.get("ENVIRONMENT", "LAPA_DATABASE_PROTOCOL")
-config_str_lapa_database_ip = config.get("ENVIRONMENT", "LAPA_DATABASE_IP")
-config_int_lapa_database_port = int(config.get("ENVIRONMENT", "LAPA_DATABASE_PORT"))
+config_str_lapa_database_protocol = str(ldict_configuration["ENVIRONMENT"]["LAPA_DATABASE_PROTOCOL"])
+config_str_lapa_database_ip = str(ldict_configuration["ENVIRONMENT"]["LAPA_DATABASE_IP"])
+config_int_lapa_database_port = int(ldict_configuration["ENVIRONMENT"]["LAPA_DATABASE_PORT"])
