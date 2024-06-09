ed6fb9e55f372487428435e8d9005e5c8fc0e4ee
thepmsquare <impmmmm@gmail.com>
2024-06-09 10:38:54 +0530

    :memo: update example.py

--- example.py
+++ example.py
@@ -4,10 +4,10 @@ lapa_database_helper = LAPADatabaseHelper()
 
 # Example: Insert Rows
 insert_data = [
-    {"device_encrypted_mac_address": "example"},
+    {"test_text": "example"},
 ]
 insert_output = lapa_database_helper.insert_rows(
-    insert_data, "lapa_testing", "authentication", "device"
+    insert_data, "lapa_testing", "public", "test"
 )
 print(insert_output)
 
@@ -16,23 +16,23 @@ get_filters = {}
 get_output = lapa_database_helper.get_rows(
     get_filters,
     "lapa_testing",
-    "authentication",
-    "device",
+    "public",
+    "test",
     ignore_filters_and_get_all=True,
 )
 print(get_output)
 
 # Example: Edit Rows
-edit_data = {"device_encrypted_mac_address": "edited"}
-edit_filters = {"device_encrypted_mac_address": "example"}
+edit_data = {"test_text": "edited"}
+edit_filters = {"test_text": "example"}
 edit_output = lapa_database_helper.edit_rows(
-    edit_data, edit_filters, "lapa_testing", "authentication", "device"
+    edit_data, edit_filters, "lapa_testing", "public", "test"
 )
 print(edit_output)
 
 # Example: Delete Rows
-delete_filters = {"device_encrypted_mac_address": "edited"}
+delete_filters = {"test_text": "edited"}
 delete_output = lapa_database_helper.delete_rows(
-    delete_filters, "lapa_testing", "authentication", "device"
+    delete_filters, "lapa_testing", "public", "test"
 )
 print(delete_output)
